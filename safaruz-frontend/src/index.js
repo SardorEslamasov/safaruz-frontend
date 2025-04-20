99770ee3cbb33e18549912b0513d7678847aaeb6
SardorEslamasov <eslamasovsardor@gmail.com>
2025-04-20 20:38:46 +0500

    Commit

--- safaruz-frontend/src/index.js
+++ safaruz-frontend/src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
 import { AuthProvider } from './context/AuthContext';
-import { ChakraProvider } from '@chakra-ui/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </ChakraProvider>
   </React.StrictMode>
 );
